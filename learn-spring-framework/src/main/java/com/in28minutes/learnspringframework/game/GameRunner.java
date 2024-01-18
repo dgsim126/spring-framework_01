df74e3e660fbf3137903c03e793bdd13d8498bf7
dgsim <dgsim126@gmail.com>
2024-01-19 00:51:25 +0900

    Iteration 2: Loose Coupling - interfaces

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,10 +1,9 @@
 package com.in28minutes.learnspringframework.game;
 
 public class GameRunner {
-	// private MarioGame game;
-	private SuperContraGame game;
+	private GamingConsole game;
 	
-	public GameRunner(SuperContraGame game) {
+	public GameRunner(GamingConsole game) {
 		this.game= game;
 	}
 
