41c12a6c690e4fce1fd2bee410a4fd157602b4a5
dgsim <dgsim126@gmail.com>
2024-01-19 18:51:33 +0900

    Bean

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App01_GamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App01_GamingBasicJava.java
@@ -5,15 +5,15 @@ import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.PacmanGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01_GamingBasicJava {
 
 	public static void main(String[] args) {
-		// var game= new MarioGame();
-		// var game= new SuperContraGame();
-		var game= new PacmanGame();
+		var game= new PacmanGame(); // 1: Object Creation
 		
+		var gameRunner= new GameRunner(game); 
+			// 2: Object Creation + Wiring of Dependencies(의존성 연결)
+			// Game is a Dependency of GameRunner 
 		
-		var gameRunner= new GameRunner(game);
 		gameRunner.run();
 	}
 
